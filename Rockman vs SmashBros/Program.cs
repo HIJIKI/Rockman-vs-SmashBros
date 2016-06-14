647908abe349749d20d23ddf153b25177e45a7d6
HIJIKI <hijiki_seaweed@yahoo.co.jp>
2016-06-14 13:43:57 +0900

    First Commit.

---
+++ Rockman vs SmashBros/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rockman_vs_SmashBros
+{
+    /// <summary>
+    /// メインクラス
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// エントリーポイント
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
